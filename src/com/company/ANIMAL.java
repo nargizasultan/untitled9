bdaeac761f0f0eaf64633f260c37a7666369c68c
nargiza <sultanmuratova500@gmail.com>
2022-02-21 19:05:57 +0600

    Initial commit

---
+++ src/com/company/ANIMAL.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public abstract class ANIMAL {
+    private String name;
+
+    public ANIMAL(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/com/company/Dog.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Dog extends ANIMAL{
+
+    public Dog(String name) {
+        super(name);
+    }
+}
